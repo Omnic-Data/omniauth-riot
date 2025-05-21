bccda5b74dd7a95f0b9193e0ba703fa03b9edc20
unknown <gabrielfitzpatrick.cs@gmail.com>
2025-05-21 15:47:42 -0400

    Map public/private keys

--- lib/omniauth/strategies/riot.rb
+++ lib/omniauth/strategies/riot.rb
@@ -6,20 +6,18 @@ module OmniAuth
     class Riot < OmniAuth::Strategies::OAuth2
       option :region, 'us'
       option :client_options, {
-        :scope => 'openid'
+        scope: 'openid'
       }
 
       def client
-        # Setup urls based on region option
-        if !options.client_options.has_key(:authorize_url)
-          options.client_options[:authorize_url] = "https://#{getHost(options.region)}/oauth/authorize"
-        end
-        if !options.client_options.has_key(:token_url)
-          options.client_options[:token_url] = "https://#{getHost(options.region)}/oauth/token"
-        end
-        if !options.client_options.has_key(:site)
-          options.client_options[:site] = "https://#{getHost(options.region)}/"
-        end
+        # Map Jumpstart-style keys to standard OAuth2 keys
+        options.client_id ||= options[:public_key] || options[:api_key]
+        options.client_secret ||= options[:private_key]
+
+        # Setup Riot OAuth2 endpoints
+        options.client_options[:authorize_url] ||= "https://#{getHost(options.region)}/oauth/authorize"
+        options.client_options[:token_url]     ||= "https://#{getHost(options.region)}/oauth/token"
+        options.client_options[:site]          ||= "https://#{getHost(options.region)}/"
 
         super
       end
@@ -31,24 +29,19 @@ module OmniAuth
       def authorize_params
         super.tap do |params|
           %w[scope client_options].each do |v|
-            if request.params[v]
-              params[v.to_sym] = request.params[v]
-            end
+            params[v.to_sym] = request.params[v] if request.params[v]
           end
         end
       end
 
       uid { raw_info['id'].to_s }
 
-      info do
-        raw_info
-      end
+      info { raw_info }
 
       def raw_info
         return @raw_info if @raw_info
 
         access_token.options[:mode] = :query
-
         @raw_info = access_token.get('oauth/userinfo').parsed
       end
 
